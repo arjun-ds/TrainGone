3ff45b4e0d2b4e0bcca5fb13d6823383c70a2184
arjun-ds <87582270+arjun-ds@users.noreply.github.com>
2023-12-01 01:53:37 -0800

    infinite scroll v1

--- A3_2_copy/app/_layout.js
+++ A3_2_copy/app/_layout.js
@@ -4,7 +4,10 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="videoInfiniteScroll" />
+      <Stack.Screen
+        name="videoInfiniteScroll"
+        options={{ headerShown: false }}
+      />
     </Stack>
   );
 }
--- A3_2_copy/app/videoInfiniteScroll.js
+++ A3_2_copy/app/videoInfiniteScroll.js
@@ -1,4 +1,12 @@
-import { Button, StyleSheet, Text, View, Image, Pressable } from "react-native";
+import {
+  FlatList,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+} from "react-native";
 import { colors } from "../assets/Themes/colors";
 import { millisToMinutesAndSeconds } from "../utils";
 import { Stack } from "expo-router/stack";
@@ -6,13 +14,52 @@ import { useNavigation, useRouter, useLocalSearchParams } from "expo-router";
 import { Video, ResizeMode } from "expo-av";
 import { Themes } from "../assets/Themes";
 import * as React from "react";
+import VideoView from "./VideoView";
+import { ScrollView } from "react-native-gesture-handler";
+import { Dimensions } from "react-native";
+import { useState } from "react";
 
 export default function videoInfiniteScroll({}) {
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+
   const router = useRouter();
   const params = useLocalSearchParams();
 
-  const video = React.useRef(null);
-  const [status, setStatus] = React.useState({});
+  // //CHANGE  THIS LINE
+  // const videos = [
+  //   {
+  //     videoName: "background",
+  //     file: require("../background.mp4"),
+  //   },
+  //   {
+  //     videoName: "background2",
+  //     file: require("../background2.mp4"),
+  //   },
+  //   {
+  //     videoName: "background3",
+  //     file: require("../background3.mp4"),
+  //   },
+  // ];
+
+  //BEGIN CODE FROM https://stackoverflow.com/questions/72851324/how-to-make-expo-av-video-to-take-needed-inside-a-flatlist
+  const defaultScreenRatio =
+    Dimensions.get("window").width / Dimensions.get("window").height;
+
+  // Use the screenDefaultRatio to render the video before the video is loaded
+  const [videoRatio, setVideoRatio] = useState(defaultScreenRatio);
+
+  // Update the videoRatio right after we know the video natural size
+  const updateVideoRatioOnDisplay = (videoDetails) => {
+    const { width, height } = videoDetails.naturalSize;
+    const newVideoRatio = width / height;
+
+    setVideoRatio(newVideoRatio);
+  };
+  //END CODE FROM https://stackoverflow.com/questions/72851324/how-to-make-expo-av-video-to-take-needed-inside-a-flatlist
+
+  // const video = React.useRef(null);
+  // const [status, setStatus] = React.useState({});
 
   return (
     <>
@@ -25,25 +72,51 @@ export default function videoInfiniteScroll({}) {
         }}
       />
       <View style={styles.container}>
-        <Video
-          ref={video}
-          style={styles.video}
-          source={require("../background.mp4")}
-          useNativeControls
-          resizeMode={ResizeMode.CONTAIN}
-          isLooping
-          onPlaybackStatusUpdate={(status) => setStatus(() => status)}
-        />
-        <View style={styles.buttons}>
-          <Button
-            title={status.isPlaying ? "Pause" : "Play"}
-            onPress={() =>
-              status.isPlaying
-                ? video.current.pauseAsync()
-                : video.current.playAsync()
-            }
+        {/* <FlatList
+          data={videos}
+          renderItem={({ item, index }) => (
+            <VideoView videoName={item.videoName} />
+          )}
+          keyExtractor={(item) => item.id}
+        /> */}
+        <ScrollView>
+          <Video
+            ref={video}
+            style={{ aspectRatio: videoRatio }} // https://stackoverflow.com/questions/72851324/how-to-make-expo-av-video-to-take-needed-inside-a-flatlist
+            resizeMode="contain"
+            source={require("../background.mp4")}
+            useNativeControls
+            // resizeMode={ResizeMode.CONTAIN}
+            isLooping
+            shouldPlay
+            onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+            onReadyForDisplay={updateVideoRatioOnDisplay} //https://stackoverflow.com/questions/72851324/how-to-make-expo-av-video-to-take-needed-inside-a-flatlist
           />
-        </View>
+          <Video
+            ref={video}
+            style={{ aspectRatio: videoRatio }} // https://stackoverflow.com/questions/72851324/how-to-make-expo-av-video-to-take-needed-inside-a-flatlist
+            resizeMode="contain"
+            source={require("../background2.mp4")}
+            useNativeControls
+            // resizeMode={ResizeMode.CONTAIN}
+            isLooping
+            shouldPlay
+            onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+            onReadyForDisplay={updateVideoRatioOnDisplay} //https://stackoverflow.com/questions/72851324/how-to-make-expo-av-video-to-take-needed-inside-a-flatlist
+          />
+          <Video
+            ref={video}
+            style={{ aspectRatio: videoRatio }} // https://stackoverflow.com/questions/72851324/how-to-make-expo-av-video-to-take-needed-inside-a-flatlist
+            resizeMode="contain"
+            source={require("../background3.mp4")}
+            useNativeControls
+            // resizeMode={ResizeMode.CONTAIN}
+            isLooping
+            shouldPlay
+            onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+            onReadyForDisplay={updateVideoRatioOnDisplay} //https://stackoverflow.com/questions/72851324/how-to-make-expo-av-video-to-take-needed-inside-a-flatlist
+          />
+        </ScrollView>
       </View>
     </>
   );
